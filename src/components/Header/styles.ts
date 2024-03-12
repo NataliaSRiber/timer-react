0a92098b8412b53add62b3dcd6c8131b79e421da
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-03-12 17:03:52 -0300

    style: add styling to Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  Justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      color: ${(props) => props.theme['gray-100']};
+      /* cria a borda e deixa transparente e no hover, modifica para a cor verde */
+      
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+    }
+  }
+`
